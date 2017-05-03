af9097936ee665dd765bbd751ec5f3c89321af9b
Corol Corona <liberty410410@gmail.com>
2017-05-03 18:05:10 +0800

    nextlink
    nextlink

--- spider_main.py
+++ spider_main.py
@@ -2,11 +2,17 @@ import urllib2
 import urllib
 import os
 from BeautifulSoup import BeautifulSoup
-def getAllImageLink():
-    html = urllib2.urlopen('http://sc.chinaz.com/tupian/fengyetupian.html').read()
+
+url='http://sc.chinaz.com/tupian/fengyetupian.html'
+nextlink=''
+
+def getAllImageLink(url):
+    
+    html = urllib2.urlopen(url).read()
     soup = BeautifulSoup(html)
 
     liResult = soup.findAll('div',attrs={"class":"box picblock col3"})
+    print url
     print len(liResult)
 
     for li in liResult:
@@ -15,8 +21,17 @@ def getAllImageLink():
             link = image.get('src2')
             imageName = image.get('alt')
             filesavepath = '/Users/corolcorona/desktop/picture/%s.jpg' % imageName
-            urllib.urlretrieve(link,filesavepath)
+                #urllib.urlretrieve(link,filesavepath)
+    try:
+        nextHtml = soup.findAll('a',attrs={"class":"nextpage"})[0].get('href')
+        if nextHtml:
+           nextlink='http://sc.chinaz.com/tupian/'+nextHtml
+           getAllImageLink(nextlink)
+
+    except Exception,e:
+        print '---end---'
+
 
 
 if __name__ == '__main__':
-    getAllImageLink()
+    getAllImageLink(url)
